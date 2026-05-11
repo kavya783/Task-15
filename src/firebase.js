e70ee0d8be5238bf36440dfb478244ccbbd95c41
kavya <kavya.actimize@gmail.com>
2026-05-11 15:36:33 +0530

    update this file

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBP5S6xUldCa9BOPYYFH3g0sQCt2cBnPFM",
-  authDomain: "react-website-a6ea5.firebaseapp.com",
-  projectId: "react-website-a6ea5",
-  storageBucket: "react-website-a6ea5.appspot.com",
-  messagingSenderId: "861287992612",
-  appId: "1:861287992612:web:60001e83319f2c5f8c77b1"
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};;
 
 const app = initializeApp(firebaseConfig);
 
